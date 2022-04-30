2f022db128e2aa300c4968eae6000bb0c3772394
Torstein Eliassen <32542379+gravlaks@users.noreply.github.com>
2022-04-30 16:50:53 -0700

    use autograd for differentiation

--- one_layer.py
+++ one_layer.py
@@ -1,5 +1,5 @@
 import torch
-from torch.autograd import Variable
+from torch.autograd import Variable, grad
 import numpy as np
 torch.manual_seed(1)
 x = torch.randn((1, 4))#([[0.1, 0.2, 0.3]])
@@ -15,7 +15,7 @@ y_pred = torch.nn.Sigmoid()(W@x.T+b)
 n = x.shape[1]
 print(y_pred)
 
-
+variable_count = (n+1)*output_dim
 
 T = np.zeros((output_dim, (n+1)*output_dim))
 print(T.shape, y_pred.shape)
@@ -25,7 +25,20 @@ for i in range(output_dim):
     print(i, vec)
     length = vec.shape[0]
     T[i, i*(length): (i+1)*(length)]= vec
-y_pred = y_pred.detach().numpy()
-Jac = y_pred*(1-y_pred)*T
+y_pred_np = y_pred.detach().numpy()
+Jac = y_pred_np*(1-y_pred_np)*T
 print(Jac)
 
+Jac = torch.zeros((output_dim, variable_count ))
+print("delYdelW", grad(y_pred[0], W, retain_graph=True))
+print(grad(y_pred[0], b, retain_graph=True))
+
+for i, y in enumerate(y_pred):
+    del_y_del_w = grad(y, W, retain_graph=True)[0].flatten()
+
+    del_y_del_b = grad(y, b, retain_graph=True)[0].flatten()
+    dim = del_y_del_w.shape[0]
+    Jac[i, :dim] = del_y_del_w
+    Jac[i, dim:] = del_y_del_b
+print(Jac)
+    
