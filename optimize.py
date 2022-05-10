1fb5748cf6dd51c6f887827897fc75dfa31d37c7
Torstein Eliassen <32542379+gravlaks@users.noreply.github.com>
2022-05-10 10:39:14 -0700

    add comments to optimize.py

--- optimize.py
+++ optimize.py
@@ -6,17 +6,28 @@ from modular_nn import NN, get_initial_params
 from tqdm import tqdm
 
 def get_block(nn, X_t, a, y):
-    
-    g = nn.forward(a, X_t)
+    """
+    Retrieves Jacobian for single datapoint and 
+    calculates one block of the least squares objective:
 
-    jac = nn.jac(a, X_t)
-    #X_flat_t = nn.flatten(X_t)
+    ||g(x_t, ai) + J(x, ai)delta_x  - yi||_2^2
+    ||J(x, ai)delta_x - (g(x_t, ai)+yi)||_2^2 
     
-    #print(-g.flatten()+y.flatten())
+    ||Aidelta_x -bi||_2^2
+
+    returns Ai, bi
+    
+    """
+    g = nn.forward(a, X_t)
+    jac = nn.jac(a, X_t)
+
     return jac, -g.flatten() + y.flatten()
 
 def take_step(nn, X_t, A, Y, lambd):
-    
+    """
+    Take one step in least squares direction
+    Line 4-5 in Ergen
+    """
     N, m = Y.shape
     N, n = A.shape
     A_ls = np.zeros((len(Y[0])*len(Y),X_t.shape[0] ))
@@ -34,14 +45,24 @@ def take_step(nn, X_t, A, Y, lambd):
     
     return X_upd
 
-def optimize(nn, X0,  A, Y, lambd=0.0001, epsilon = 1e-5):
-   
+def optimize(g, X0,  A, Y, lambd=0.0001, epsilon = 1e-5):
+    """
+    g: generic class that provides a forward function and jacobian function
+    X0 : initial parameter guess for parameters in g
+    A (N, n): data
+    Y (N, m): labels
+    lambd: regression parameter for Gauss Newton
+    epsilon: tolerance for when to quit iteration
+
+    Algorithm 2 (currently without projection)
+    Gauss Newton for training Neural network
+    """
 
     X_t = X0
     MAX_ITER = 100
     for k in tqdm(range(MAX_ITER)):
         X_tm1 = np.copy(X_t)
-        X_t = take_step(nn, X_t, A, Y, lambd)
+        X_t = take_step(g, X_t, A, Y, lambd)
 
         if np.linalg.norm(X_tm1-X_t) <= epsilon:
             break
@@ -52,6 +73,8 @@ if __name__ == '__main__':
     N, n = 100, 2
     m = 1
     torch.manual_seed(1)
+
+    #Construct Neural Network
     Ws0, bs0 = get_initial_params(hidden_layer_count=0, m=m, n=n, hidden_neurons=1)
     Ws_true, bs_true = get_initial_params(hidden_layer_count=0, m=m, n=n, hidden_neurons=1)
     X_true = (Ws_true, bs_true)
@@ -59,6 +82,7 @@ if __name__ == '__main__':
 
     nn = NN(X0)
 
+    ## Generate X and Y labels
     A = np.random.randn(N, n)
     A[0, :] = np.ones((n,))
     Y = np.zeros((N, m))
@@ -67,13 +91,14 @@ if __name__ == '__main__':
         y_pred =  nn.forward(a, nn.flatten(X_true)).flatten()
         Y[i, :] = y_pred
     
-
+    ## Run algorithm 2
     X_est = optimize(nn, nn.flatten(X0), A, Y)
+
+
+
+    ## Print results
     print("X_true", nn.flatten(X_true))
     print("X_est", X_est)
-
-
-
     for i in range(N):
         a = A[i, :].reshape((n, 1))
         y_true = nn.forward(a, nn.flatten(X_true)).flatten()
