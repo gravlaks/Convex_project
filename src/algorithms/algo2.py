fix scaling of gaussian variance

src/algorithms/algo2.py
@@ -80,7 +80,7 @@ class Stepper():
         error_vec = self.nn.forward(A, self.X_t + delt)-Y.flatten()
         nabla_f = 2*A_ls.T@error_vec #+ 2*self.lambd*delt
 
-        min_lambd, max_lambd = 1e-15, 500
+        min_lambd, max_lambd = 1e-15, 1
         lambd_multiplier = 4
         while new_error > old_error + alpha*t*(nabla_f).T@delt :
             i+=1
@@ -140,7 +140,8 @@ class Stepper():
         t2 = time.time()
         if self.optimization_method=="Gaussian":
             k = int(self.batch_size*0.85)
-            S = np.random.randn(k, A_ls.shape[0]) / (A_ls.shape[0])
+            #extra_scaling = 10
+            S = np.random.randn(k, A_ls.shape[0]) / np.sqrt(A_ls.shape[0])
             delt = lsmr(S@A_ls, S@b_ls, damp=np.sqrt(self.lambd), atol=1e-4)[0]
 
         elif self.optimization_method == "Random columns":

src/mnist_conv_gaussian.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     torch.manual_seed(0)
 
     ## Load MNIST Dataset 
-    N=500
+    N=800
     (train_X, train_y), (test_X, test_y) = get_data_classifier(N=N, linear_input=False)
     ## Get initial neural network parameters 
     m = train_y.shape[1]
@@ -32,26 +32,29 @@ if __name__ == '__main__':
     print("Parameters", X0.shape)
 
     ## Do Gauss Newton
-    MAX_TIME = 100
-    print("Train Accuracy", get_accuracy(nn_gn, train_X, train_y))
-    print("Test Accuracy", get_accuracy(nn_gn, test_X, test_y))
-    X_est,train_errors_gaussian, _, timer, _ = optimize(nn_gn, X0, train_X, train_y, batch_size=150, max_time=MAX_TIME, backtrack=True,
-                        optimization_method="Gaussian", optim_params={"keep_prob":1})
+    MAX_TIME = 60
+    BATCH_SIZE=300
+
+    for i in range(1):
+        print("Train Accuracy", get_accuracy(nn_gn, train_X, train_y))
+        print("Test Accuracy", get_accuracy(nn_gn, test_X, test_y))
+        X_est,train_errors_gaussian, _, timer, _ = optimize(nn_gn, X0, train_X, train_y, batch_size=BATCH_SIZE, max_time=MAX_TIME, backtrack=True,
+                            optimization_method="Gaussian", optim_params={"keep_prob":1,"momentum": 0})
 
 
-    print("Train Accuracy", get_accuracy(nn_gn, train_X, train_y))
-    print("Test Accuracy", get_accuracy(nn_gn, test_X, test_y))
-    X_est,train_errors_05, _, timer, _  = optimize(nn_gn, X0, train_X, train_y, batch_size=150, max_time=MAX_TIME, backtrack=True,
-                        optimization_method="Random columns", optim_params={"keep_prob":1})
-    
-    ## Print results
-    losses = [train_errors_gaussian,train_errors_05]
-    labels = ["Algo 2: Gaussian", "Algo 2: Keep prob = 1"]
-    print("Train Accuracy", get_accuracy(nn_gn, train_X, train_y))
-    print("Test Accuracy", get_accuracy(nn_gn, test_X, test_y))
-    plot_mult(losses, labels,  "plots/Gaussian comparison", MAX_TIME, "Algo 2: Digit 10dim classification")
+        print("Train Accuracy", get_accuracy(nn_gn, train_X, train_y))
+        print("Test Accuracy", get_accuracy(nn_gn, test_X, test_y))
+        X_est,train_errors_05, _, timer, _  = optimize(nn_gn, X0, train_X, train_y, batch_size=BATCH_SIZE, max_time=MAX_TIME, backtrack=True,
+                            optimization_method="Random columns", optim_params={"keep_prob":1})
+        
+        ## Print results
+        losses = [train_errors_gaussian,train_errors_05]
+        labels = ["Algo 2: Gaussian", "Algo 2: Keep prob = 1"]
+        print("Train Accuracy", get_accuracy(nn_gn, train_X, train_y))
+        print("Test Accuracy", get_accuracy(nn_gn, test_X, test_y))
+        plot_mult(losses, labels,  "data/temp/Gaussian comparison", MAX_TIME, "Algo 2: Digit 10dim classification")
 
-    
+        
 
 
 

src/mnist_conv_nn_classifier.py
@@ -1,6 +1,7 @@
 from functions.PyTorchGN import NN_GN
 from neural_networks.Conv2 import Conv2
 from neural_networks.Conv2_classifier import Conv2_Classifier
+from neural_networks.Conv3_classifier import Conv3_Classifier
 from neural_networks.FC1 import FC1
 from neural_networks.FC2 import FC2
 from neural_networks.Conv1 import Conv1
@@ -18,7 +19,7 @@ if __name__ == '__main__':
     torch.manual_seed(0)
 
     ## Load MNIST Dataset 
-    N=800
+    N=400
     (train_X, train_y), (test_X, test_y) = get_data_classifier(N=N, linear_input=False)
 
     ## Get initial neural network parameters 
@@ -33,7 +34,7 @@ if __name__ == '__main__':
     print("Parameters", X0.shape)
 
     ## Do Gauss Newton
-    MAX_TIME = 120
+    MAX_TIME = 5
     print("Train Accuracy", get_accuracy(nn_gn, train_X, train_y))
     print("Test Accuracy", get_accuracy(nn_gn, test_X, test_y))
     X_est,train_errors_1, _ , timer1, _,= optimize(nn_gn, X0, train_X, train_y, batch_size=300, max_time=MAX_TIME, backtrack=True,
@@ -57,10 +58,10 @@ if __name__ == '__main__':
     sgd_losses_lr05 = stochastic_gradient_descent(train_X, train_y,  nn_gn=nn_gn, max_time=MAX_TIME, batch_size=300, lr=0.5)
     print("Train Accuracy", get_accuracy(nn_gn, train_X, train_y))
     print("Test Accuracy", get_accuracy(nn_gn, test_X, test_y))
-    # plot_timer(timer1, title="Timer: keep_prob=1")
-    # plot_timer(timer05, title="Timer: keep_prob=0.5")
-    # plot_timer(timer01, title="Timer: keep_prob=0.1")
-    # plot_timer(timer001, title="Timer: keep_prob=0.001")
+    plot_timer(timer1, title="Timer: keep_prob=1")
+    plot_timer(timer05, title="Timer: keep_prob=0.5")
+    plot_timer(timer01, title="Timer: keep_prob=0.1")
+    plot_timer(timer001, title="Timer: keep_prob=0.01")
     sgd_losses_lr01 = stochastic_gradient_descent(train_X, train_y, nn_gn=nn_gn, max_time=MAX_TIME, batch_size=300, lr=0.1)
     print("Train Accuracy", get_accuracy(nn_gn, train_X, train_y))
     print("Test Accuracy", get_accuracy(nn_gn, test_X, test_y))

src/momentum.py
@@ -1,6 +1,7 @@
 from functions.PyTorchGN import NN_GN
 from neural_networks.Conv2 import Conv2
 from neural_networks.Conv2_classifier import Conv2_Classifier
+from neural_networks.Conv3_classifier import Conv3_Classifier
 from neural_networks.FC1 import FC1
 from neural_networks.FC2 import FC2
 from neural_networks.Conv1 import Conv1
@@ -18,14 +19,14 @@ if __name__ == '__main__':
     torch.manual_seed(0)
 
     ## Load MNIST Dataset 
-    N=400
+    N=1400
     (train_X, train_y), (test_X, test_y) = get_data_classifier(N=N, linear_input=False)
 
     ## Get initial neural network parameters 
     m = train_y.shape[1]
     n = train_X.shape[1]
 
-    neural_network = Conv2_Classifier()
+    neural_network = Conv3_Classifier()
     #summary(neural_network, input_size = (1, 28, 28))
     neural_network.float()
     nn_gn = NN_GN(neural_network)
@@ -33,8 +34,8 @@ if __name__ == '__main__':
     print("Parameters", X0.shape)
 
     ## Do Gauss Newton
-    MAX_TIME = 120
-    EXPERIMENTS_COUNT = 4
+    MAX_TIME = 60
+    EXPERIMENTS_COUNT = 1
 
     losses = []
 
@@ -42,15 +43,15 @@ if __name__ == '__main__':
     plt.figure()
     for _ in range(EXPERIMENTS_COUNT):
         X_est,train_errors, _ , timer1, _,= optimize(nn_gn, X0, train_X, train_y, batch_size=300, max_time=MAX_TIME, backtrack=True,
-                        optimization_method="Random columns", optim_params={"keep_prob":0.01, "momentum": 0.5}, visualize=False)
+                        optimization_method="Random columns", optim_params={"keep_prob":0.1, "momentum": 0}, visualize=False)
         Ts = np.linspace(0, MAX_TIME, len(train_errors))
         plt.plot(Ts, train_errors, color='b')
 
-        X_est,train_errors, _ , timer1, _,= optimize(nn_gn, X0, train_X, train_y, batch_size=300, max_time=MAX_TIME, backtrack=True,
-                        optimization_method="Random columns", optim_params={"keep_prob":0.01, "momentum": 0}, visualize=False)
+        # X_est,train_errors, _ , timer1, _,= optimize(nn_gn, X0, train_X, train_y, batch_size=300, max_time=MAX_TIME, backtrack=True,
+        #                 optimization_method="Random columns", optim_params={"keep_prob":0.01, "momentum": 0}, visualize=False)
 
-        Ts = np.linspace(0, MAX_TIME, len(train_errors))
-        plt.plot(Ts, train_errors, color='g')
+        # Ts = np.linspace(0, MAX_TIME, len(train_errors))
+        # plt.plot(Ts, train_errors, color='g')
         sgd_losses_lr05 = stochastic_gradient_descent(train_X, train_y,  nn_gn=nn_gn, max_time=MAX_TIME, batch_size=300, lr=0.5)
 
         Ts = np.linspace(0, MAX_TIME, len(sgd_losses_lr05))